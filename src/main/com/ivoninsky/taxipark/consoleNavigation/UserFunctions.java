cfd6fd6dd83d38c114314376334dd7388dad6f26
Dmitry <dmitry.ivoninsky@gmail.com>
2019-12-23 23:31:43 +0300

    finishing user functions, refactoring code

--- src/main/com/ivoninsky/taxipark/consoleNavigation/UserFunctions.java
+++ src/main/com/ivoninsky/taxipark/consoleNavigation/UserFunctions.java
@@ -3,6 +3,7 @@ package com.ivoninsky.taxipark.consoleNavigation;
 import com.ivoninsky.taxipark.cars.Car;
 import com.ivoninsky.taxipark.interfaces.TaxiPark;
 import com.ivoninsky.taxipark.taxiparks.Offer;
+import com.ivoninsky.taxipark.validators.IntegerValidator;
 
 import java.util.List;
 import java.util.Scanner;
@@ -31,6 +32,11 @@ public class UserFunctions {
                 case("1"):
                     System.out.println("Input count of kilometers: ");
                     String s = sc.next();
+                    IntegerValidator validator = new IntegerValidator();
+                    while (!validator.validate(s)){
+                        System.out.println("Input valid count of kilometers: ");
+                        s = sc.next();
+                    }
                     countOfKilometers = Integer.parseInt(s);
                     break;
                 case("2"):
@@ -38,16 +44,20 @@ public class UserFunctions {
                         System.out.println("Сount of kilometers not indicated");
                         break;
                     }
+                    else if (taxiPark.getListOfCars().size() == 0){
+                        System.out.println("No added cars to taxipark");
+                        break;
+                    }
                     System.out.println("Input class of the trip (Econom/Comfort/Business): ");
-
                     String classOfTrip = sc.next();
+                    while (!classOfTrip.equals("Econom") && !classOfTrip.equals("Comfort") && !classOfTrip.equals("Business")){
+                        System.out.println("Input correct class of trip(Econom/Comfort/Business): ");
+                        classOfTrip = sc.next();
+                    }
                     selectClassOfTrip(classOfTrip);
-
-                    System.out.println("Cost of trip" + taxiPark.calculateTotalCostOrder(car, countOfKilometers));
+                    System.out.println("Cost of trip = $" + taxiPark.calculateTotalCostOrder(car, countOfKilometers));
                     break;
                 case("3"):
-                    System.out.println("Type \"confirm\" to confirm an order" +
-                            "or type \"exit\" to select new car");
                     if (car == null){
                         System.out.println("No car selected");
                         break;
@@ -56,6 +66,17 @@ public class UserFunctions {
                         System.out.println("Сount of kilometers not indicated");
                         break;
                     }
+                    System.out.println("Type \"confirm\" to confirm an order " +
+                            "or type \"exit\" to select new car");
+                    String isConfirm = sc.next();
+                    while (!isConfirm.equals("confirm") && !isConfirm.equals("exit")){
+                        System.out.println("Input \"confirm\" or \"exit\"");
+                        isConfirm = sc.next();
+                    }
+                    if (isConfirm.equals("exit")){
+                        break;
+                    }
+                    System.out.println("offer");
                     getOffer();
                     break;
                 case("~"):
@@ -94,9 +115,9 @@ public class UserFunctions {
     }
 
     private Offer getOffer() {
-        Offer offer = new Offer();
-
-
+        Offer offer = new Offer(car, taxiPark.calculateTotalCostOrder(car, countOfKilometers));
+        offer.writeOfferToFile();
+        System.out.println("Your file at ~/Projects/taxiPark/offers");
         return null;
 
     }
